26e716dfa08bc3f721a1d4bce9be29e0e11d21fa
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-03-03 19:27:48 +0300

    fix: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stek = [];
+    
+  for (item of str)
+  {
+      for (bracket of bracketsConfig)
+      {
+          if(item === bracket[0] && item !== bracket[1])
+          {
+              stek.push(item)
+          }
+          else if(item === bracket[1] && item !== bracket[0])
+          {
+               let currentItem  = stek.pop()
+               if(currentItem !== bracket[0]){
+                   return false;
+               }
+
+          }
+          else if (item === bracket[0] && item === bracket[1])
+          {
+              if(stek[stek.length-1] === item)
+              {
+                  stek.pop()
+              }
+              else{
+                  stek.push(item)
+              }
+          }
+          
+      }
+  }
+  return !stek.length
 }
